081883246bd90d6415216ed7e565368a962adf0e
Ernesto Villegas <ernesto.nmn.villegas@gmail.com>
2023-01-17 12:30:34 -0600

    updates contacts app

---
+++ src/ContactApp.java
@@ -0,0 +1,34 @@
+package src;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+public class ContactApp {
+
+    public static void main(String[] args) throws IOException {
+        String directory = "data";
+        String filename = "contacts.txt";
+
+        Path dataDirectory = Paths.get(directory);
+        Path dataFile = Paths.get(directory, filename);
+
+        if (Files.notExists(dataDirectory)) {
+            Files.createDirectories(dataDirectory);
+        }
+
+        if (! Files.exists(dataFile)) {
+            Files.createFile(dataFile);
+        }
+
+        List<String> contactList = Arrays.asList("Jack Black | 1231231234", "Bob Joe | 1231231234");
+        Path filepath = Paths.get("data", "contacts.txt");
+        Files.write(filepath, contactList);
+
+
+
+    }
+}
---
+++ src/Contacts.java
@@ -0,0 +1,37 @@
+package src;
+
+public class Contacts {
+
+    private String name;
+    private int phone_number;
+
+    public Contacts(String name, int phone_number) {
+        this.name = name;
+        this.phone_number = phone_number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(int phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public static void viewContacts() {
+        for (int i = 0; i < contactList.size(); i += 1) {
+
+            System.out.println((i + 1) + ": " + contactList.get(i));
+        }
+    }
+
+    
+}
